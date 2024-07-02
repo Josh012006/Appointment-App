3d96937b39ac94b46574d2e67ee72e1fb8025f82
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2024-07-02 14:22:16 +0000

    Some minor changes

--- app/userpage/pat/requests/page.tsx
+++ app/userpage/pat/requests/page.tsx
@@ -6,7 +6,11 @@
 function PatRequests() {
     return(
         <div className="p-3">
-            <h1>Appointment requests</h1>
+            <h1 className="mx-auto text-2xl font-bold text-center">Mes demandes</h1>
+            <p className="text-center my-3 mx-auto">Cet onglet répertorie vos demandes en cours c&apos;est-à-dire, celles non encore confirmées ou traitées. Tant que ces demandes ne sont pas encores vues, il est possible de les annuler. Mais après, ce n&apos;est plus possible!</p>
+            <div className="flex flex-col">
+            
+            </div>
         </div>
     )
 }
